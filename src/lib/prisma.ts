9fa08549c5634afbd7f7f24a084f0942bb668c79
Eliza-Onyango <elizabethonyango9898@gmail.com>
2026-04-20 18:01:25 +0300

    merge conflicts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,13 +7,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma = 
   globalForPrisma.prisma ??
   new PrismaClient({
-<<<<<<< Updated upstream
-    log: process.env.NODE_ENV === "development"
-      ? ["error", "warn"]
-      : ["error"],
-=======
     log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
->>>>>>> Stashed changes
   });
 
 if (process.env.NODE_ENV !== "production") {
